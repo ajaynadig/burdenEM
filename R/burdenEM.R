02c995dcc498f28c1f82e4ef8917ed8800384021
Ajay Nadig <ajay.g.nadig@gmail.com>
2023-11-14 10:49:18 -0500

    Initial commit for burdenEM function and usage script.
    Ajay confirmed that usage script runs as intended for poisson data/uniform model case.

---
+++ R/burdenEM.R
@@ -0,0 +1,90 @@
+burdenEM <- function(mixture_params, model_type = 'uniform', effect_estimate = NULL,
+                     effect_se = NULL, case_count = NULL, case_rate = NULL, p_value = NULL,
+                     features = NULL) {
+
+  # Validate inputs
+  stopifnot(is.vector(mixture_params))
+
+  no_cpts <- length(mixture_params)
+
+  # Handling effect estimates and standard errors
+  if (!is.null(effect_estimate)) {
+    stopifnot(is.null(case_count))  # Ensure that case_count is not provided
+    if (is.null(effect_se)) {
+      stopifnot(!is.null(p_value))  # p_value must be specified if effect_se is not
+      z_score <- qnorm(1 - p_value / 2) * sign(effect_estimate)
+      effect_se <- abs(effect_estimate / z_score)
+    }
+    stopifnot(length(effect_se) == length(effect_estimate))
+    data_type <- 'normal'
+    no_tests <- length(effect_estimate)
+  } else {
+    stopifnot(!is.null(case_count))
+    stopifnot(model_type == 'uniform')  # Only uniform is supported for Poisson
+    if (is.null(case_rate)) {
+      stopifnot(!is.null(p_value))
+      # Implement poisson_rate function based on the MATLAB code
+      case_rate <- poisson_rate(case_count, p_value)
+    }
+    stopifnot(length(case_rate) == length(case_count))
+    data_type <- 'poisson'
+    no_tests <- length(case_count)
+  }
+
+  # Compute likelihood
+  likelihood <- matrix(0, nrow = no_tests, ncol = no_cpts)
+  mu_grid <- seq(0.05, 1, by = 0.1)
+  if (model_type == 'uniform' && data_type == 'normal') {
+    for (kk in 1:no_cpts) {
+      mu <- mu_grid * mixture_params[kk]
+      sigma <- effect_se
+      likelihood[, kk] <- rowMeans(dnorm(effect_estimate, mean = mu, sd = sigma))
+    }
+  } else if (model_type == 'uniform' && data_type == 'poisson') {
+    for (kk in 1:no_cpts) {
+      # Expand case_count into a matrix by replicating the vector along the columns
+      case_count_matrix <- replicate(length(mu_grid),case_count)
+
+      # Calculate the rate for each mu value in the grid and each case count
+      rate <- case_rate * t(replicate(length(case_rate),exp(mu_grid * mixture_params[kk])))
+
+      # Compute the likelihood for each case count and rate combination
+      likelihoods <- dpois(case_count_matrix, rate)
+
+      # Calculate the average likelihood for each test across the grid of mu values
+      likelihood[, kk] <- rowMeans(likelihoods)
+    }
+  } else {
+    stop('Options for model_type are uniform or normal')
+  }
+
+  if (is.null(features)) {
+    features <- matrix(1, nrow = no_tests, ncol = 1)
+  }
+
+  stopifnot(all(rowSums(features) == 1) & all(features >= 0))
+
+  # EM algorithm
+  coefs <- matrix(1, nrow = ncol(features), ncol = no_cpts)
+  for (rep in 1:5) {
+    weights <- features %*% coefs
+    posteriors <- weights * likelihood
+    posteriors <- posteriors / replicate(ncol(posteriors),rowSums(posteriors))
+    coefs <- solve(t(features) %*% features) %*% t(features) %*% posteriors
+    print(coefs)
+  }
+
+  return(list(coefs = coefs, mixture_params = mixture_params))
+}
+
+poisson_rate <- function(x, p, steps = 100) {
+  lambda <- x
+  factx <- factorial(x)
+
+  for (step in 1:steps) {
+    cdf <- ppois(x, lambda, lower.tail = FALSE)
+    lambda <- lambda - (cdf - (1 - p)) / (exp(-lambda) * lambda^x / factx)
+  }
+
+  return(lambda)
+}
---
+++ usage/burdenEM_testusage.R
@@ -0,0 +1,46 @@
+nn <- 10000
+normal_model <- FALSE
+
+# Generating features matrix
+features <- matrix(runif(nn), nrow = nn, ncol = 2)
+features[, 2] <- 1 - features[, 1]
+
+model_params <- c(-4, 1, 0, 1, 4)
+coefs <- matrix(c(.01, 0, .04, 0, .9, 1, .04, 0, .01, 0), nrow = 2, byrow = TRUE)
+
+# Mixture component assignments
+weights <- features %*% coefs
+weights <- weights / replicate(ncol(weights),rowSums(weights))
+
+cpt_param <- numeric(nn)
+for (ii in 1:nn) {
+  cpt_param[ii] <- sample(model_params, 1, prob = weights[ii, ], replace = TRUE)
+}
+
+# Effect sizes
+beta <- runif(nn)
+beta <- beta * cpt_param
+
+# Noise in the effect estimates
+if (normal_model) {
+  noise_var <- runif(nn)
+  beta_hat <- beta + sqrt(noise_var) * rnorm(nn)
+
+  # Estimation
+  system.time({
+    coefs_est <- burdenEM(model_params, effect_estimate = beta_hat, effect_se = sqrt(noise_var),
+                          features = features, model_type = 'uniform')
+  })
+} else {
+  case_rate <- 10 * runif(nn)
+  lambda <- case_rate * exp(beta)
+  case_count <- rpois(nn, lambda)
+
+  # Estimation
+  system.time({
+    coefs_est <- burdenEM(model_params, case_count = case_count, case_rate = case_rate,
+                          features = features, model_type = 'uniform')
+  })
+}
+
+print(coefs_est)
